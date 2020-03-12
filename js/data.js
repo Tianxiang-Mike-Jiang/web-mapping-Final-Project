af9d72931c375f4b87c4517914d9f7b70a12a0c5
Mike Jiang <60728792+Tianxiang-Mike-Jiang@users.noreply.github.com>
2020-03-11 22:13:46 -0400

    1

---
+++ js/data.js
@@ -0,0 +1,205 @@
+var nbastadiumData= [
+  {
+    "StadiumName": "State Farm Arena",
+    "longitude": -84.3985184,
+    "latitude": 33.7572935,
+    "website": "statefarmarena.com",
+    "Team": "Atlanta Hawks"
+  },
+  {
+    "StadiumName": "TD Garden",
+    "longitude": -71.06434,
+    "latitude": 42.3662019,
+    "website": "tdgarden.com",
+    "Team": "Boston Celtics"
+  },
+  {
+    "StadiumName": "Barclays Center",
+    "longitude": -73.9776096,
+    "latitude": 40.6826505,
+    "website": "barclayscenter.com",
+    "Team": "Brooklyn Nets"
+  },
+  {
+    "StadiumName": "Spectrum Center",
+    "longitude": -80.8414977,
+    "latitude": 35.2251853,
+    "website": "spectrumcentercharlotte.com",
+    "Team": "Charlotte Hornets"
+  },
+  {
+    "StadiumName": "United Center",
+    "longitude": -87.6763699,
+    "latitude": 41.8806948,
+    "website": "unitedcenter.com",
+    "Team": "Chicago Bulls"
+  },
+  {
+    "StadiumName": "Quicken Loans Arena",
+    "longitude": -81.6904069,
+    "latitude": 41.4964837,
+    "website": "rocketmortgagefieldhouse.com",
+    "Team": "Cleveland Cavaliers"
+  },
+  {
+    "StadiumName": "American Airlines Center",
+    "longitude": -96.8173592,
+    "latitude": 32.7848939,
+    "website": "americanairlinescenter.com",
+    "Team": "Dallas Mavericks"
+  },
+  {
+    "StadiumName": "Pepsi Center",
+    "longitude": -105.009904,
+    "latitude": 39.7486641,
+    "website": "pepsicenter.com",
+    "Team": "Denver Nuggets"
+  },
+  {
+    "StadiumName": "The Palace of Auburn Hills",
+    "longitude": -83.2447494,
+    "latitude": 42.698533,
+    "website": "",
+    "Team": "Detroit Pistons"
+  },
+  {
+    "StadiumName": "Chase Center",
+    "longitude": -122.3895203,
+    "latitude": 37.7679315,
+    "website": "chasecenter.com",
+    "Team": "Golden State Warriors"
+  },
+  {
+    "StadiumName": "Toyota Center",
+    "longitude": -93.5971038,
+    "latitude": 34.9922688,
+    "website": "houstontoyotacenter.com",
+    "Team": "Houston Rockets"
+  },
+  {
+    "StadiumName": "STAPLES Center",
+    "longitude": -118.2694481,
+    "latitude": 34.0430219,
+    "website": "staplescenter.com",
+    "Team": "Los Angeles Lakers/ Los Angeles Clippers"
+  },
+  {
+    "StadiumName": "FedExForum",
+    "longitude": -90.0527804,
+    "latitude": 35.1381462,
+    "website": "fedexforum.com",
+    "Team": "Memphis Grizzlies"
+  },
+  {
+    "StadiumName": "American Airlines Arena",
+    "longitude": -80.189163,
+    "latitude": 25.7814062,
+    "website": "aaarena.com",
+    "Team": "Miami Heat"
+  },
+  {
+    "StadiumName": "Fiserv Forum",
+    "longitude": -87.9195802,
+    "latitude": 43.0450841,
+    "website": "fiservforum.com",
+    "Team": "Milwaukee Bucks"
+  },
+  {
+    "StadiumName": "Target Center",
+    "longitude": -93.278183,
+    "latitude": 44.9795319,
+    "website": "targetcenter.com",
+    "Team": "Minnesota Timberwolves"
+  },
+  {
+    "StadiumName": "Smoothie King Center",
+    "longitude": -90.0842508,
+    "latitude": 29.9490397,
+    "website": "smoothiekingcenter.com",
+    "Team": "New Orleans Pelicans"
+  },
+  {
+    "StadiumName": "Madison Square Garden",
+    "longitude": -73.9956327,
+    "latitude": 40.7505085,
+    "website": "msg.com",
+    "Team": "New York Knicks"
+  },
+  {
+    "StadiumName": "Chesapeake Energy Arena",
+    "longitude": -97.5173078,
+    "latitude": 35.463429,
+    "website": "chesapeakearena.com",
+    "Team": "Oklahoma City Thunder"
+  },
+  {
+    "StadiumName": "Amway Center",
+    "longitude": -81.3860475,
+    "latitude": 28.5392261,
+    "website": "amwaycenter.com",
+    "Team": "Orlando Magic"
+  },
+  {
+    "StadiumName": "Wells Fargo Center",
+    "longitude": -75.1741735,
+    "latitude": 39.9012056,
+    "website": "wellsfargocenterphilly.com",
+    "Team": "Philadelphia 76ers"
+  },
+  {
+    "StadiumName": "US Airways Center",
+    "longitude": -112.0740501,
+    "latitude": 33.4458798,
+    "website": "",
+    "Team": "Phoenix Suns"
+  },
+  {
+    "StadiumName": "Moda Center",
+    "longitude": -122.6690363,
+    "latitude": 45.5315688,
+    "website": "rosequarter.com",
+    "Team": "Portland Trail Blazers"
+  },
+  {
+    "StadiumName": "Sleep Train Arena",
+    "longitude": -121.5203224,
+    "latitude": 38.649043,
+    "website": "",
+    "Team": "Sacramento Kings"
+  },
+  {
+    "StadiumName": "AT&T Center",
+    "longitude": -98.4396753,
+    "latitude": 29.4271568,
+    "website": "attcenter.com",
+    "Team": "San Antonio Spurs"
+  },
+  {
+    "StadiumName": "Scotiabank Arena",
+    "longitude": -79.3812929,
+    "latitude": 43.64347,
+    "website": "scotiabankarena.com",
+    "Team": "Toronto Raptors"
+  },
+  {
+    "StadiumName": "Energy Solutions Arena",
+    "longitude": -111.9032761,
+    "latitude": 40.7682681,
+    "website": "vivintarena.com",
+    "Team": "Utah Jazz"
+  },
+  {
+    "StadiumName": "Capital One Arena",
+    "longitude": -77.0229949,
+    "latitude": 38.898065,
+    "website": "capitalonearena.com",
+    "Team": "Washington Wizards"
+  },
+  {
+    "StadiumName": "Bankers Life Fieldhouse",
+    "longitude": -86.1577307,
+    "latitude": 39.7640475,
+    "website": "bankerslifefieldhouse.com",
+    "Team": "Indiana Pacers"
+  }
+]
