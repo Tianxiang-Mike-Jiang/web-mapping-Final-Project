a273243d1f19ebd999e7a0b161573d276bb7772f
Mike Jiang <60728792+Tianxiang-Mike-Jiang@users.noreply.github.com>
2020-03-23 17:34:40 -0400

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,37 @@
+
+// My mapboxGL token
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZWppYW5nMTEwIiwiYSI6ImNrNnR2bnZmNTAzZ3Azb3Izd3ExOW9obmUifQ.1gPI_5WBiSt3GwqrGk_S8g';
+
+// Initial center point and zoom level
 var initialCenterPoint = [-98.1875746, 39.1911332]
 var initialZoom =3.4
 
+// Create an object to hold the initialization options for a map
 var initOptions = {
   container: 'map-container',
   style: 'mapbox://styles/mapbox/light-v10',
   center: initialCenterPoint,
   zoom: initialZoom,
 }
+
+// Create new map
 var map = new mapboxgl.Map(initOptions);
 
+// Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
+
 
 var filterGroup = document.getElementById('filter-group');
 
 map.on('load', function() {
 // Add a GeoJSON source containing place coordinates and information.
-map.addSource('nbastadiumData', {
-'type': 'geojson',
-'data': nbastadiumData
+map.addSource('nbastadium', {
+  type: 'geojson',
+  data: 'data\nbastadium.geojson',
 });
 
-nbastadiumData.forEach(function(nbastadiumEntry) {
-
-
-new mapboxgl.Marker()
-      .setLngLat([nbastadiumEntry.longitude, nbastadiumEntry.latitude])
-      .setPopup(new mapboxgl.Popup({ offset: 25}) // add popups
-       .setHTML(`<div id='logo' style="background-image:url('${nbastadiumEntry.Logo}'); height：100%;background-repeat:no-repeat; background-position:center; background-size:contain "></div><br>
-
-                <b>${nbastadiumEntry.StadiumName}</b> <br>
-                <b>Built Year:</b> ${nbastadiumEntry.Year}<br>
-                <b>NBA Team:</b> ${nbastadiumEntry.Team}<br>
-                <b>Team Website</b> <br><a href="http://www.${nbastadiumEntry.website}">${nbastadiumEntry.website}</a><br>
-                <b>Divisions:</b> ${nbastadiumEntry.Divisions}<br>
-                <b>Conference:</b> ${nbastadiumEntry.Conference}<br>
-                <div id='image-zoom' style="background-image:url('${nbastadiumEntry.Picture}'); height：100%;background-repeat:no-repeat; background-position:center; background-size:contain "></div>`))
-      .addTo(map);
-
-var symbol = nbastadiumEntry['Conference'];
+nbastadium.features.forEach(function(feature) {
+var symbol = feature.properties['conference'];
 var layerID = 'poi-' + symbol;
 
 // Add a layer for this symbol type if it hasn't been added already.
@@ -46,12 +39,12 @@ if (!map.getLayer(layerID)) {
 map.addLayer({
 'id': layerID,
 'type': 'symbol',
-'source': 'nbastadiumData',
+'source': 'nbastadium',
 'layout': {
 'icon-image': symbol + '-15',
 'icon-allow-overlap': true
 },
-'filter': ['==', 'Conference', symbol]
+'filter': ['==', 'conference', symbol]
 });
 
 // Add checkbox and label elements for the layer.
@@ -77,6 +70,8 @@ e.target.checked ? 'visible' : 'none'
 }
 });
 });
+
+
 
 
   $('#ATL').on('click', function() {
